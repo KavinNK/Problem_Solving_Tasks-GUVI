b5a47de3601c326486dd596183ad9681c1ccfddf
KavinNK <KavinNK@github.com>
2020-04-01 03:20:44 +0530

    Javascript

---
+++ Javascript/Tasks/Object.js
@@ -0,0 +1,53 @@
+var tech_spec = 
+{
+    dimensions:{
+        overall_length:3500,
+        overall_width:1600,
+        overall_height:1490,
+        wheelbase:2360,
+        track_width :
+        {
+            front:1405,
+            rear:1406
+        }
+
+    },
+    capacities:{
+        seating_capacity:5,
+        fuel_tank_capacity:35
+    },
+    engine:{
+        type:"KB-Series",
+        number_of_cylinders:3,
+        number_of_valves:12,
+        capacity:998,
+        bore_x_stroke:"73.0 x 79.5",
+        compression_ratio:"10:1",
+        maximum_power:"67/6200",
+        maximum_torque:"90/3500",
+        fuel_distribution:"Multipoint Injection"
+    },
+    transmission:{
+        type:"5-speed MT"
+    },
+    chassis:{
+        steering:["Rack&Pinion","Power assisted"],
+        brakes:{
+            front:"Ventilated disks",
+            rear:"Drums"
+        },
+        suspension:{
+            front:"MacPherson strut & coil spring",
+            rear:"Isolated trailling& coil spring"
+        },
+        shock_absorbers:"Gas filled",
+        tyre:"155/80R13"
+    },
+    weights:{
+        kerb_weight:"860-880",
+        gross_vehicle_weight:1320
+    }
+
+
+}
+
