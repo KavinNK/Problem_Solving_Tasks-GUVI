719641f999837a1cb975f39757a697a3a4b6b3fc
KavinNK <KavinNK@github.com>
2020-04-01 18:34:11 +0530

    Datatype

--- Javascript/Tasks/Datatype.js
+++ Javascript/Tasks/Datatype.js
@@ -1,2 +1,17 @@
 var a = 35
+var b = "GUVI"
+var c = undefined
+var d = {
+    first_name:"Kavin"
+    last_name:"Arasu"
+}
+var e = null
+
+var f = ["This","is","a","array","of","strings"]
+
 console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
