bcdcc4ac7dcf6bbc491eff3d0b59f0f35a8deb8b
KavinNK <KavinNK@github.com>
2020-04-05 23:20:01 +0530

    Datatype fix

--- Javascript/Tasks/4_Calc.js
+++ Javascript/Tasks/4_Calc.js
@@ -1,2 +1,27 @@
 //Write a program to create a calculator with all arithmetic operations has functions;
 
+function sum(x,y){
+    return(x+y)
+
+}
+function sub(x,y){
+    return(y-x)
+
+}
+function prod(x,y){
+    return(x*y)
+
+}
+function div(x,y){
+    return(x/y)
+
+}
+
+
+var a = 10
+var b = 12
+
+console.log(sum(a,b))
+console.log(sub(a,b))
+console.log(prod(a,b))
+console.log(div(a,b))
