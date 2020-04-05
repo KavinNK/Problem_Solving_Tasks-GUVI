84e61bb3fa8af18e76ee26f50edf4a4f90f8ebb5
KavinNK <KavinNK@github.com>
2020-04-05 23:11:39 +0530

    05-04-2020

--- Javascript/Tasks/5_Sum.js
+++ Javascript/Tasks/5_Sum.js
@@ -1,11 +1,12 @@
-var a = 10;
 
+var a = 10;
+//5c. Creating a function to return a random variable
 let random = function () {
     return Math.floor(Math.random() * 10)
 }
-
+//5b. Creating a function to return sum.
 function sum(x,y){
-    return(a+random())
+    return(x+y)//passing the values to 
 
 }
 
